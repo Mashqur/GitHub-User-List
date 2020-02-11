f0066d9e5fb5c388cabbb0cd2f60ca5cb6ac0268
Mashqur Habib Himel <himel@Mashqurs-Mac-mini.local>
2020-02-12 04:22:34 +0600

    initial commit

---
+++ GitHubUserList/GitHubUserList/RestApiHandler.swift
@@ -0,0 +1,9 @@
+//
+//  RestApiHandler.swift
+//  GitHubUserList
+//
+//  Created by Mashqur Habib Himel on 12/2/20.
+//  Copyright © 2020 mashqur. All rights reserved.
+//
+
+import Foundation
---
+++ GitHubUserList/GitHubUserList/UserCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserCellTableViewCell.swift
+//  GitHubUserList
+//
+//  Created by Mashqur Habib Himel on 12/2/20.
+//  Copyright © 2020 mashqur. All rights reserved.
+//
+
+import UIKit
+
+class UserCellTableViewCell: UITableViewCell {
+
+
+}
